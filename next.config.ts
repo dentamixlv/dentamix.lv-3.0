12a3edc1841fd4f383642fd47b18bf0db0551d65
dentamixlv <dentamix.lv@gmail.com>
2026-06-25 10:33:56 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -43,9 +43,9 @@ const nextConfig: NextConfig = {
             key: "Content-Security-Policy",
             value: [
               "default-src 'self';",
-              "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://*.prismic.io https://*.convex.cloud https://*.posthog.com https://*.i.posthog.com;",
-              "connect-src 'self' https://*.prismic.io https://*.convex.cloud wss://*.convex.cloud https://generativelanguage.googleapis.com https://*.posthog.com https://*.i.posthog.com;",
-              "img-src 'self' data: blob: https://*.prismic.io https://images.unsplash.com https://images.prismic.io https://*.posthog.com https://*.i.posthog.com;",
+              "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://*.prismic.io https://*.convex.cloud https://*.posthog.com https://*.i.posthog.com https://www.googletagmanager.com https://*.google-analytics.com;",
+              "connect-src 'self' https://*.prismic.io https://*.convex.cloud wss://*.convex.cloud https://generativelanguage.googleapis.com https://*.posthog.com https://*.i.posthog.com https://*.google-analytics.com https://*.analytics.google.com;",
+              "img-src 'self' data: blob: https://*.prismic.io https://images.unsplash.com https://images.prismic.io https://*.posthog.com https://*.i.posthog.com https://*.google-analytics.com https://www.googletagmanager.com;",
               "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://*.posthog.com https://*.i.posthog.com;",
               "font-src 'self' data: https://fonts.gstatic.com https://*.posthog.com https://*.i.posthog.com;",
               "media-src 'self' https://*.prismic.io https://*.prismic.io/dentamix-v30/ https://*.posthog.com https://*.i.posthog.com;",
