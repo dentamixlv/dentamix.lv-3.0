1e04f64533d251f17cccbde7853702ae2c976250
dentamixlv <dentamix.lv@gmail.com>
2026-06-25 10:38:31 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -44,13 +44,13 @@ const nextConfig: NextConfig = {
             value: [
               "default-src 'self';",
               "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://*.prismic.io https://*.convex.cloud https://*.posthog.com https://*.i.posthog.com https://www.googletagmanager.com https://*.google-analytics.com;",
-              "connect-src 'self' https://*.prismic.io https://*.convex.cloud wss://*.convex.cloud https://generativelanguage.googleapis.com https://*.posthog.com https://*.i.posthog.com https://*.google-analytics.com https://*.analytics.google.com;",
-              "img-src 'self' data: blob: https://*.prismic.io https://images.unsplash.com https://images.prismic.io https://*.posthog.com https://*.i.posthog.com https://*.google-analytics.com https://www.googletagmanager.com;",
+              "connect-src 'self' https://*.prismic.io https://*.convex.cloud wss://*.convex.cloud https://generativelanguage.googleapis.com https://*.posthog.com https://*.i.posthog.com https://*.google-analytics.com https://*.analytics.google.com https://*.doubleclick.net https://*.google.com;",
+              "img-src 'self' data: blob: https://*.prismic.io https://images.unsplash.com https://images.prismic.io https://*.posthog.com https://*.i.posthog.com https://*.google-analytics.com https://www.googletagmanager.com https://*.doubleclick.net https://*.google.com;",
               "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://*.posthog.com https://*.i.posthog.com;",
               "font-src 'self' data: https://fonts.gstatic.com https://*.posthog.com https://*.i.posthog.com;",
               "media-src 'self' https://*.prismic.io https://*.prismic.io/dentamix-v30/ https://*.posthog.com https://*.i.posthog.com;",
               "frame-src 'self' https://www.google.com;",
-              `report-uri https://eu.i.posthog.com/report/?token=${process.env.NEXT_PUBLIC_POSTHOG_KEY || "phc_z7oTfiM6pyqbrJ6AxJAEpfGEnADMLctohsjsGWaZC9Z3"}`
+              `report-uri https://eu.i.posthog.com/report/?token=${process.env.NEXT_PUBLIC_POSTHOG_KEY || "phc_z7oTfiM6pybrJ6AxJAEpfGEnADMLctohsjsGWaZC9Z3"}`
             ].join(" "),
           },
         ],
